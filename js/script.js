5c59409f5385a82564bf117ede6a92e9d73ccd53
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-10 10:58:47 +0300

    changed effect

--- js/script.js
+++ js/script.js
@@ -1,58 +1,23 @@
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
+$(document).ready(function(){
+$(".type-wrap").typed({
+  strings: ["Website?", "Logo?", "orice alt design?"],
+  stringsElement: null,
+  // typing speed
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 0,
+  shuffle: false,
+  backDelay: 2000,
+  fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 1500, // milliseconds
+  loop: false,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  attr: null,
+  contentType: 'html'
+  
+});
 
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="text-effect">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i < elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .text-effect { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
+})
