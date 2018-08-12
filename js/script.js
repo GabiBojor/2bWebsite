107a271a7e564514686bccb7113d0cb7c1839229
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-12 15:08:49 +0300

    redirect

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-      if($(window).scrollTop() > 200) {
-        $('.container-top').addClass('scrolled-top');
-      }
-      else{
-        $('.container-top').removeClass('scrolled-top');
-      }
+  $(document).ready(function(){
+    $(".type-wrap").typed({
+      strings: ["Website?", "Logo?", "orice alt design?","Website?", "Logo?", "orice alt design?", "Website?"],
+      stringsElement: null,
+      typeSpeed: 0,
+      startDelay: 0,
+      backSpeed: 0,
+      shuffle: false,
+      backDelay: 2000,
+      fadeOut: false,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 1500, 
+      loop: false,
+      loopCount: false,
+      showCursor: true,
+      cursorChar: "|",
+      attr: null,
+      contentType: 'html'
     });
-$(".type-wrap").typed({
-  strings: ["Website?", "Logo?", "orice alt design?","Website?", "Logo?", "orice alt design?", "Website?"],
-  stringsElement: null,
-  typeSpeed: 0,
-  startDelay: 0,
-  backSpeed: 0,
-  shuffle: false,
-  backDelay: 2000,
-  fadeOut: false,
-  fadeOutClass: 'typed-fade-out',
-  fadeOutDelay: 1500, 
-  loop: false,
-  loopCount: false,
-  showCursor: true,
-  cursorChar: "|",
-  attr: null,
-  contentType: 'html'
-});
+  })
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 200) {
+      $('.container-top').addClass('scrolled-top');
+    }
+    else{
+      $('.container-top').removeClass('scrolled-top');
+    }
+  });
 
-})
+
+     function getOrder() { 
+      var home_param = document.getElementById('order-input').value;
+      // var loc = window.location.pathname
+     var newLink = "about-us.html/"
+      // += `&${home_param}`
+     location.pathname = newLink;
+    }
