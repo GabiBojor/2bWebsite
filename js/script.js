977e07dbbd9dbd55968e14af2e4529f27a52e571
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-14 20:49:16 +0300

    decode

--- js/script.js
+++ js/script.js
@@ -39,11 +39,9 @@
 
 
   function getOrder() { 
-    alert('test');
     var home_param = document.getElementById('order-input').value.toLowerCase();
     var link = `order.html#${home_param}`;
     var decode = decodeURIComponent(link)
-    alert(link);
     location.pathname = decode;
   }
 
