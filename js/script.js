e70ea5e38af25ff2642490b1304cc83883300a67
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-15 18:03:55 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
   $(document).ready(function(){
 
     if($('.check-id').length > 0){
+        $('.container-top').addClass('scrolled-top');
       $('.next-step').click(function(e){
         e.preventDefault();
         if ($('.active-section').next('.step').length) {
