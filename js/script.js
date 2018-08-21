ab13ac17045294dccd41340f5b2601eee36c10eb
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-21 11:44:40 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
   $(document).ready(function(){
-       $('.projects-menu > a').click(function(e){
-      e.preventDefault();
-      $('.projects-menu > a').removeClass('active-btn')
-      $(this).addClass('active-btn');
-      var link = $(this).attr('rel');
-        $('.our-projects').fadeOut();
-        $('div.' + link).fadeIn();
+    if($('.projects-wrapper').length > 0){
+      $('.projects-wrapper').filterizr();
+      $('.projects-menu > li').click(function(e){
+        e.preventDefault();
+        $('.projects-menu > li').removeClass('active-btn')
+        $(this).addClass('active-btn');
+      })
+    }
 
-    })
     if($('.order-wrapper').length > 0){
-        $('.container-top').addClass('active-top');
+      $('.container-top').addClass('active-top');
       $('.next-step').click(function(e){
         e.preventDefault();
         if ($('.active-section').next('.step').length) {
@@ -24,7 +24,7 @@
 
     }
     $(".type-wrap").typed({
-      strings: ["Website?", "Logo?", "orice alt design?","Website?", "Logo?", "orice alt design?", "Website?"],
+      strings: ["Website?", "Logo?", "orice alt design?"],
       stringsElement: null,
       typeSpeed: 0,
       startDelay: 0,
@@ -54,9 +54,9 @@
 
 
   function getOrder() {
-      var option_one = document.getElementById('order-input').value;
-      var alert_popup = '<div class="alert"><div class="right"><a class="close-alert" onclick="closePopup()"><i class="fas fa-times-circle"></i></a></div>PLease bla bla</div> <div class="overlay" onclick="closePopup()"></div> ';
-      if(option_one == 0){
+    var option_one = document.getElementById('order-input').value;
+    var alert_popup = '<div class="alert"><div class="right"><a class="close-alert" onclick="closePopup()"><i class="fas fa-times-circle"></i></a></div>PLease bla bla</div> <div class="overlay" onclick="closePopup()"></div> ';
+    if(option_one == 0){
       document.body.insertAdjacentHTML( 'beforeend', alert_popup );
       document.querySelector('.overlay').style.display = 'block';
       setTimeout(function(){
